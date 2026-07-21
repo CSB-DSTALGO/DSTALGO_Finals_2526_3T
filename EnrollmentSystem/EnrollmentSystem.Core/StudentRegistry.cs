610540ca88409798d3d04f5d12a32e32b8786cd8
Kentaro A. Shiozawa <kentaro.shiozawa@benilde.edu.ph>
2026-07-21 19:59:53 +0800

    Implemented CustomArrayList.cs and StudentRegistry.cs

--- EnrollmentSystem/EnrollmentSystem.Core/StudentRegistry.cs
+++ EnrollmentSystem/EnrollmentSystem.Core/StudentRegistry.cs
@@ -8,16 +8,59 @@ public class StudentRegistry
 
     public int Count => _students.Count;
 
-    public void RegisterStudent(Student student) => throw new NotImplementedException();
-    public bool UnregisterStudent(int index) => throw new NotImplementedException();
-    public bool RemoveStudent(string id) => throw new NotImplementedException();
-    public Student GetStudentAt(int index) => throw new NotImplementedException();
+    public void RegisterStudent(Student student) => _students.Add(student);//throw new NotImplementedException();
+    //public bool UnregisterStudent(int index) => throw new NotImplementedException();
+    public bool UnregisterStudent(int index)
+    {
+        try
+        {
+            _students.RemoveAt(index);
+            return true;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return false;
+        }
+    }
+    //public bool RemoveStudent(string id) => throw new NotImplementedException();
+    public bool RemoveStudent(string id)
+    {
+        for (int i = 0; i < _students.Count; i++)
+        {
+            if (_students.Get(i).Id.ToString() == id)
+            {
+                _students.RemoveAt(i);
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+    
+    public Student GetStudentAt(int index) => _students.Get(index);//throw new NotImplementedException();
 
     // Hint: Calculate average GPA of all registered students
-    public double CalculateAverageGpa() => throw new NotImplementedException();
+    //public double CalculateAverageGpa() => throw new NotImplementedException();
+    public double CalculateAverageGpa()
+    {
+        if (_students.Count == 0)
+        {
+            return 0;
+        }
+
+        double total = 0;
+
+        for (int i = 0; i < _students.Count; i++)
+        {
+            total += _students.Get(i).Gpa;
+        }
+
+        return total / _students.Count;
+    }
 
     // Hint: Delegate search and sort to CustomArrayList<T>
-    public int SearchStudent(Student student) => throw new NotImplementedException();
-    public void SortStudentsByGpa() => throw new NotImplementedException();
-    public int GetStudentCount() => throw new NotImplementedException();
+    public int SearchStudent(Student student) => _students.Search(student);//throw new NotImplementedException();
+    public void SortStudentsByGpa() => _students.Sort();//throw new NotImplementedException();
+    public int GetStudentCount() => _students.Count;//throw new NotImplementedException();
 }
