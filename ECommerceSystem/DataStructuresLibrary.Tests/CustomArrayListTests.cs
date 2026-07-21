90ad0989c859ade14f74ebe19464fcc593fe08b5
NanDalida <ronan.dalidaii@benilde.edu.ph>
2026-07-21 14:30:33 +0800

    -Changed dotnet to v9 -Updated DataStructuresLibrary>CustomArrayList.cs -Updated DataStructureLibraryTest>CustomArrayListTest.cs

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
@@ -9,34 +9,91 @@ public class CustomArrayListTests
     public void Add_ShouldIncreaseCountAndStoreItems()
     {
         // TODO: Implement test for Add and Get indexing
-        throw new NotImplementedException();
+        CustomArrayList<int> test = new CustomArrayList<int>();
+
+        test.Add(10);
+        test.Add(20);
+        test.Add(30);
+
+        Assert.Equal(3, test.Count);
+        Assert.Equal(10, test.Get(0));
+        Assert.Equal(20, test.Get(1));
+        Assert.Equal(30, test.Get(2));
+
+        //throw new NotImplementedException();
     }
 
     [Fact]
     public void Remove_ShouldShiftElementsCorrectly()
     {
         // TODO: Implement test verifying element removal and index shifting
-        throw new NotImplementedException();
+        CustomArrayList<int> test = new CustomArrayList<int>();
+
+        test.Add(10);
+        test.Add(20);
+        test.Add(30);
+
+        bool removed = test.Remove(20);
+
+        Assert.True(removed);
+        Assert.Equal(2, test.Count);
+        Assert.Equal(10, test.Get(0));
+        Assert.Equal(30, test.Get(1));
+
+
+        //throw new NotImplementedException();
     }
 
     [Fact]
     public void Search_ShouldReturnCorrectIndex_WhenItemExists()
     {
         // TODO: Test Search returning zero-based index for existing element
-        throw new NotImplementedException();
+        CustomArrayList<int> test = new CustomArrayList<int>();
+
+        test.Add(10);
+        test.Add(20);
+        test.Add(30);
+
+        int index = test.Search(20);
+        Assert.Equal(1, index);
+
+
+        //throw new NotImplementedException();
     }
 
     [Fact]
     public void Search_ShouldReturnMinusOne_WhenItemDoesNotExist()
     {
         // TODO: Test Search returning -1 when element is absent
-        throw new NotImplementedException();
+        CustomArrayList<int> test = new CustomArrayList<int>();
+
+        test.Add(10);
+        test.Add(20);
+        test.Add(30);
+
+        int index = test.Search(50);
+
+        Assert.Equal(-1, index);
+
+
+        //throw new NotImplementedException();
     }
 
     [Fact]
     public void Sort_ShouldOrderElementsInAscendingSequence()
     {
         // TODO: Test Sort ordering an unsorted CustomArrayList<int>
-        throw new NotImplementedException();
+        CustomArrayList<int> test = new CustomArrayList<int>();
+
+        test.Add(8);
+        test.Add(7);
+        test.Add(3);
+
+        test.Sort();
+        Assert.Equal(3, test.Get(0));
+        Assert.Equal(7, test.Get(1));
+        Assert.Equal(8, test.Get(2));
+
+        //throw new NotImplementedException();
     }
 }
--- ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -5,19 +5,99 @@ public class CustomArrayList<T> where T : IComparable<T>
     private T[] _items;
     public int Count { get; private set; }
 
+    //Initial Capacity is Set to 4 and the Count is 0
     public CustomArrayList(int initialCapacity = 4)
     {
         _items = new T[initialCapacity];
+        Count = 0;
     }
-    //
     
-    public void Add(T item) => throw new NotImplementedException();
-    public bool Remove(T item) => throw new NotImplementedException();
-    public T Get(int index) => throw new NotImplementedException();
+    //Dynamically Resizes the array when it is full
+    //new Array called newItems is created which is a temporary array that copies everything inside the items array
+    //_items is updated to reference the new array
+    private void Resize()
+    {
+        T[] tItems = new T[_items.Length * 2]; //create new array with double the length
 
-    
-    public int Search(T item) => throw new NotImplementedException();
+        for(int i = 0; i < Count; i++)//loop that copies elements in old to new array
+        {
+            tItems[i] = _items[i];
+        }
+        _items = tItems; // replace old array with the new array
+    }
 
-    
-    public void Sort() => throw new NotImplementedException();
+    //add new element at the end of the _items array
+    public void Add(T item)
+    {
+        //first checks if the max capacity of the array is reached
+        if(Count == _items.Length)
+        {
+            Resize();
+        }
+        //inserts the new element 
+        _items[Count] = item;
+
+        Count++; //increases the number of stored items
+    }
+    public bool Remove(T item)
+    {
+        int index = Search(item);//find index of item
+
+        //if item aint found it cant be removed
+        if(index == -1)
+        {
+            return false;
+        }
+
+        //shift all elements to the left after one position is cleared
+        for (int i = index; i < Count - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _items[Count - 1] = default!;
+        Count--; //reduces the count of array
+        return true;
+
+    }
+    //find specific element
+    public T Get(int index)
+    {
+        if(index < 0 || index >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return _items[index];//return the element of the given index
+    }
+
+    //linear search to check each element in the array
+    public int Search(T item)
+    {
+        for(int i = 0; i < Count; i++)
+        {
+            if (_items[i].CompareTo(item) == 0)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //bubble sort!
+    public void Sort()
+    {
+        for(int i =0; i < Count - 1; i++)//repeats until sorted
+        {
+            for(int j = 0; j < Count - i - 1; j++)//compare adjacent elements
+            {
+                if (_items[j].CompareTo(_items[j + 1]) > 0)//swap if wrong order
+                {
+                    T temp = _items[j];
+                    _items[j] = _items[j + 1];
+                    _items[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
