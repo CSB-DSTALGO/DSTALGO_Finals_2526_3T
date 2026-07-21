b938727cdbda3c4eb730ae230d5dec3b703059fa
Ermis-16 <cyrilnade@gmail.com>
2026-07-21 11:46:18 +0800

    Made changes to wrong file

--- EnrollmentSystem/DataStructuresLibrary/CustomQueue.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomQueue.cs
@@ -12,32 +12,62 @@ namespace DataStructuresLibrary
 
         public int Count 
         { 
-            get { throw new NotImplementedException(); } 
+            
+            get { return _count; } 
         }
 
         public CustomQueue()
         {
-            throw new NotImplementedException();
+            //Constructor
+            _items = new T[4]; //size
+            _front = 0;
+            _rear =  0;
+            _count = 0;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if(_count == _items.Length)
+            {
+                // Resize the array if it's full
+                T[] temp = new T[_items.Length * 2];
+                Array.Copy(_items, _front, temp, 0, _count);
+                _items = temp;
+                _front = 0;
+                _rear = _count;
+
+            }   
+            item = _items[_rear];
+            _rear = (_rear + 1) % _items.Length;
+            _count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            T item = _items[_front];
+            _front = (_front + 1) % _items.Length;  //wrap around
+            _count --;
+            return item;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return _items[_front];
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return _count == 0;
         }
     }
 }
