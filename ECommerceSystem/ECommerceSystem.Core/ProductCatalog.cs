4183b2494bb1ca8923c470782bf9252c4f07a5ff
garridoalgerie-kesz <garrido.algerie@gmail.com>
2026-07-21 21:04:50 +0800

    Implemented CustomArrayList with add, remove, search, and sort

--- ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
+++ ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
@@ -1,17 +1,18 @@
-namespace ECommerceSystem.Core;
-
 using DataStructuresLibrary;
 
-public class ProductCatalog
+namespace ECommerceSystem.Core
 {
-    private readonly CustomSinglyLinkedList<Product> _products = new();
+    public class ProductCatalog
+    {
+        private readonly CustomSinglyLinkedList<Product> _products = new();
 
-    public int Count => _products.Count;
+        public int Count => _products.Count;
 
-    public void AddProduct(Product product) => throw new NotImplementedException();
-    public bool RemoveProduct(Product product) => throw new NotImplementedException();
+        public void AddProduct(Product product) => throw new NotImplementedException();
+        public bool RemoveProduct(Product product) => throw new NotImplementedException();
 
-    
-    public bool SearchProduct(Product product) => throw new NotImplementedException();
-    public void SortCatalog() => throw new NotImplementedException();
+
+        public bool SearchProduct(Product product) => throw new NotImplementedException();
+        public void SortCatalog() => throw new NotImplementedException();
+    }
 }
