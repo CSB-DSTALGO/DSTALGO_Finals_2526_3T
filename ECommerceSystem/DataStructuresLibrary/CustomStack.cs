cc6e9b1f8fc68091711d1ef61167d79132e54d68
Billy B. Caling Jr. <billy.calingjr@benilde.edu.ph>
2026-07-22 19:02:24 +0800

    Implement CustomStack constructor and Push operation

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -2,10 +2,42 @@ namespace DataStructuresLibrary;
 
 public class CustomStack<T> where T : IComparable<T>
 {
+    private T[] _items;
+    private const int InitialCapacity = 4;
+
     public int Count { get; private set; }
 
-    public void Push(T item) => throw new NotImplementedException();
+    public CustomStack()
+    {
+        _items = new T[InitialCapacity];
+        Count = 0;
+    }
+
+    public void Push(T item)
+    {
+        if (item == null)
+        {
+            throw new ArgumentNullException(nameof(item));
+        }
+
+        if (Count == _items.Length)
+        {
+            T[] newArray = new T[_items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                newArray[i] = _items[i];
+            }
+
+            _items = newArray;
+        }
+
+        _items[Count] = item;
+        Count++;
+    }
+
     public T Pop() => throw new NotImplementedException();
+
     public T Peek() => throw new NotImplementedException();
 
     public int Search(T item) => throw new NotImplementedException();
