7cec8b10f0288f620834d4ef0bd044c156ea365d
tarravictor <tarra.victor.c@gmail.com>
2026-07-22 21:55:51 +0800

    Implement CustomArrayList and its unit tests

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
@@ -8,35 +8,74 @@ public class CustomArrayListTests
     [Fact]
     public void Add_ShouldIncreaseCountAndStoreItems()
     {
-        // TODO: Implement test for Add and Get indexing
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+
+        Assert.Equal(2, list.Count);
+        Assert.Equal(10, list.Get(0));
+        Assert.Equal(20, list.Get(1));
     }
 
     [Fact]
     public void Remove_ShouldShiftElementsCorrectly()
     {
-        // TODO: Implement test verifying element removal and index shifting
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        bool removed = list.Remove(20);
+
+        Assert.True(removed);
+        Assert.Equal(2, list.Count);
+        Assert.Equal(10, list.Get(0));
+        Assert.Equal(30, list.Get(1));
     }
 
     [Fact]
     public void Search_ShouldReturnCorrectIndex_WhenItemExists()
     {
-        // TODO: Test Search returning zero-based index for existing element
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        int index = list.Search(20);
+
+        Assert.Equal(1, index);
     }
 
     [Fact]
     public void Search_ShouldReturnMinusOne_WhenItemDoesNotExist()
     {
-        // TODO: Test Search returning -1 when element is absent
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        int index = list.Search(40);
+
+        Assert.Equal(-1, index);
     }
 
     [Fact]
     public void Sort_ShouldOrderElementsInAscendingSequence()
     {
-        // TODO: Test Sort ordering an unsorted CustomArrayList<int>
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(30);
+        list.Add(10);
+        list.Add(20);
+
+        list.Sort();
+
+        Assert.Equal(10, list.Get(0));
+        Assert.Equal(20, list.Get(1));
+        Assert.Equal(30, list.Get(2));
     }
 }
--- ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -10,14 +10,89 @@ public class CustomArrayList<T> where T : IComparable<T>
         _items = new T[initialCapacity];
     }
 
-    
-    public void Add(T item) => throw new NotImplementedException();
-    public bool Remove(T item) => throw new NotImplementedException();
-    public T Get(int index) => throw new NotImplementedException();
+    // Adds a new item to the end of the array list.
+    public void Add(T item)
+    {
+        if (Count == _items.Length)
+        {
+            Resize();
+        }
 
-    
-    public int Search(T item) => throw new NotImplementedException();
+        _items[Count] = item;
+        Count++;
+    }
 
-    
-    public void Sort() => throw new NotImplementedException();
+    // Removes the first matching item from the array list.
+    public bool Remove(T item)
+    {
+        int index = Search(item);
+
+        if (index == -1)
+        {
+            return false;
+        }
+
+        for (int i = index; i < Count - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _items[Count - 1] = default!;
+        Count--;
+        return true;
+    }
+
+    // Returns the item at the specified index.
+    public T Get(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+
+        return _items[index];
+    }
+
+    // Searches for an item and returns its index, or -1 if not found.
+    public int Search(T item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (_items[i].CompareTo(item) == 0)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // Sorts the array list in ascending order using Bubble Sort.
+    public void Sort()
+    {
+        for (int i = 0; i < Count - 1; i++)
+        {
+            for (int j = 0; j < Count - i - 1; j++)
+            {
+                if (_items[j].CompareTo(_items[j + 1]) > 0)
+                {
+                    (_items[j], _items[j + 1]) = (_items[j + 1], _items[j]);
+                }
+            }
+        }
+    }
+
+    // Resizes the internal array when capacity is full.
+    private void Resize()
+    {
+        int newCapacity = _items.Length == 0 ? 4 : _items.Length * 2;
+        T[] newItems = new T[newCapacity];
+
+        for (int i = 0; i < Count; i++)
+        {
+            newItems[i] = _items[i];
+        }
+
+        _items = newItems;
+    }
 }
