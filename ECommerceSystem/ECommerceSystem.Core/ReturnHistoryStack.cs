f23e5da58a0dc4cdae3f5b6d919f27dddd522f3e
barsilog <bary_23_20@yahoo.com>
2026-07-22 23:39:06 +0800

    Add CheckHistoryEmpty method to ReturnHistoryStack

--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -12,6 +12,7 @@ public class ReturnHistoryStack
     public ReturnRequest PopReturn() => throw new NotImplementedException();
     public ReturnRequest PeekLatestReturn() => throw new NotImplementedException();
 
+    public bool CheckHistoryEmpty() => throw new NotImplementedException();
     
     public int SearchReturn(ReturnRequest request) => throw new NotImplementedException();
     public void SortReturns() => throw new NotImplementedException();
