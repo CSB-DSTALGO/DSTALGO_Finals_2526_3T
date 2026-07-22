59894e0d47225b0dfbe49bbedc98f8a7b30ad566
San Jose_Francesca <franc@CHESCA>
2026-07-23 01:03:17 +0800

    Custom Stack

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -2,14 +2,56 @@ namespace DataStructuresLibrary
 {
     public class CustomStack<T> where T : IComparable<T>
     {
+        private readonly List<T> items = new List<T>();
         public int Count { get; private set; }
 
-        public void Push(T item) => throw new NotImplementedException();
-        public T Pop() => throw new NotImplementedException();
-        public T Peek() => throw new NotImplementedException();
+        public void Push(T item)
+        {
+            items.Add(item);
+            Count++;
+        }
 
-        public int Search(T item) => throw new NotImplementedException();
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
 
-        public void Sort() => throw new NotImplementedException();
+            T item = items[Count - 1];
+
+            items.RemoveAt(Count - 1);
+            Count--;
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+
+            return items[Count - 1];
+        }
+        public int Search(T item)
+        {
+            for (int i = Count - 1, depth = 1; i >= 0; i--, depth++)
+            {
+                if (items[i].CompareTo(item) == 0)
+                {
+                    return depth;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Sort()
+        {
+            items.Sort();
+            items.Reverse();
+        }
     }
 }
--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -8,6 +8,7 @@ namespace ECommerceSystem.Core
 
         public int Count => _returns.Count;
 
+<<<<<<< HEAD
         public void PushReturn(ReturnRequest request) => throw new NotImplementedException();
         public ReturnRequest PopReturn() => throw new NotImplementedException();
         public ReturnRequest PeekLatestReturn() => throw new NotImplementedException();
@@ -15,5 +16,30 @@ namespace ECommerceSystem.Core
 
         public int SearchReturn(ReturnRequest request) => throw new NotImplementedException();
         public void SortReturns() => throw new NotImplementedException();
+=======
+    public void PushReturn(ReturnRequest request)
+    {
+        _returns.Push(request);
+    }
+
+    public ReturnRequest PopReturn()
+    {
+        return _returns.Pop();
+    }
+
+    public ReturnRequest PeekLatestReturn()
+    {
+        return _returns.Peek();
+    }
+
+    public int SearchReturn(ReturnRequest request)
+    {
+        return _returns.Search(request);
+    }
+
+    public void SortReturns()
+    {
+        _returns.Sort();
+>>>>>>> 86cedef (Custom Stack)
     }
 }
