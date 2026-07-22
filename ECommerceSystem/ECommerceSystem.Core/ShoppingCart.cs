1cb994ae21256d1005630f73e02cfb263e487c09
Dannielyn Obille <elynobille@gmail.com>
2026-07-23 00:50:43 +0800

    Updated CustomArrayList, Shopping Cart, and CustomArrayList tests.

--- ECommerceSystem/ECommerceSystem.Core/ShoppingCart.cs
+++ ECommerceSystem/ECommerceSystem.Core/ShoppingCart.cs
@@ -4,17 +4,60 @@ using DataStructuresLibrary;
 
 public class ShoppingCart
 {
-    private readonly CustomArrayList<Product> _items = new();
+    private readonly CustomArrayList<Product> _items = new(); // walang product class dba..?
 
     public int Count => _items.Count;
 
-    public void AddItem(Product product) => throw new NotImplementedException();
-    public bool RemoveItem(Product product) => throw new NotImplementedException();
-    public Product GetItemAt(int index) => throw new NotImplementedException();
+    // insrrts inventory record
+    public void AddItem(Product product)
+    {
+        _items.Add(product);
+    }
 
-    public decimal CalculateTotal() => throw new NotImplementedException();
+    // remove product by index
+    public bool RemoveItem(Product product)
+    {
+        return _items.Remove(product);
+    }
 
-    
-    public int SearchItem(Product product) => throw new NotImplementedException();
-    public void SortCartByPrice() => throw new NotImplementedException();
+    // returns record by index
+    public Product GetItemAt(int index)
+    {
+        return _items.Get(index);
+    }
+
+    // shows all items
+
+    public void ShowAllItems()
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            Console.WriteLine(_items.Get(i));
+        }
+    }
+
+    // Calculates the total price of all products in the cart
+    public decimal CalculateTotal()
+    {
+        decimal total = 0;
+
+        for (int i = 0; i < _items.Count; i++)
+        {
+            total += _items.Get(i).Price;
+        }
+
+        return total;
+    }
+
+    // Searches for a product and returns its index
+    public int SearchItem(Product product)
+    {
+        return _items.Search(product);
+    }
+
+    // Sorts the cart by product price (or however Product.CompareTo() is implemented)
+    public void SortCartByPrice()
+    {
+        _items.Sort();
+    }
 }
