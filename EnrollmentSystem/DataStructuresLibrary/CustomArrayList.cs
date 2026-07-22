96773e17df02f8a8338c4764377b33b4ae5c8c46
NanDalida <ronan.dalidaii@benilde.edu.ph>
2026-07-22 19:03:51 +0800

    -Created and added code on the CUstomArrayList.cs

--- EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -10,32 +10,68 @@ namespace DataStructuresLibrary
 
         public int Count 
         { 
-            get { throw new NotImplementedException(); } 
+            get { return _count; }
         }
 
+        //creates the initial array with a capacity of 4
         public CustomArrayList()
         {
-            throw new NotImplementedException();
+            _items = new T[4];
+            _count = 0;
         }
+
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            //Check if array is full
+            if (_count == _items.Length)
+            {
+                Resize();
+            }
+
+            _items[_count] = item;//stores the new item at last index of array
+
+            _count++;//increases the index by 1
+            
         }
 
         public T Get(int index)
         {
-            throw new NotImplementedException();
+            if(index < 0 || index >= _count)//checks if the index is outside the stored element
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+
+            return _items[index];
+            
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _count)//checsk if the index is valid
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (int i = index; i < _count -1; i++)//shifts all elements by one to the left
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            _items[_count - 1] = default!;//clears the last element
+            _count--;//decreases the number of stored elemenets 
         }
 
         private void Resize()
         {
-            throw new NotImplementedException();
+            T[] tItems = new T[_items.Length * 2]; //create new array with double the length
+
+            for (int i = 0; i < Count; i++)//loop that copies elements in old to new array
+            {
+                tItems[i] = _items[i];
+            }
+            _items = tItems; // replace old array with the new array
         }
     }
 }
