56abd0b5aa0abf7e00c4ff363cad7609bfc15669
Shaira Nicole Cadatal <shairanicolce.cadatal@benilde.edu.ph>
2026-07-22 14:22:37 +0800

    Add CustomArrayList/Cadatal

--- EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -5,37 +5,67 @@ namespace DataStructuresLibrary
 {
     public class CustomArrayList<T>
     {
-        private T[] _items;
-        private int _count;
+        private T[] _items; // store elements in array
+        private int _count; // count
 
-        public int Count 
-        { 
-            get { throw new NotImplementedException(); } 
+        public int Count
+        {
+            get { return _count; } // return count of stored elements
         }
 
-        public CustomArrayList()
+        public CustomArrayList() // empty arraylist
         {
-            throw new NotImplementedException();
+            _items = new T[4]; // initial cap
+            _count = 0; // starts empty
         }
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            if (_count == _items.Length) // resize
+            {
+                Resize();
+            }
+
+            _items[_count] = item; // store item
+            _count++;
         }
 
         public T Get(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return _items[index]; // return the requested item
         }
 
         public void RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= _count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            for (int i = index; i < _count - 1; i++) // shift elem to left
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            _items[_count - 1] = default(T); // clear the last elem
+            _count--;
         }
 
         private void Resize()
         {
-            throw new NotImplementedException();
+            T[] newItems = new T[_items.Length * 2];
+
+            for (int i = 0; i < _count; i++) // copy existing items
+            {
+                newItems[i] = _items[i];
+            }
+
+            _items = newItems; // make _item the reference of the larger array
         }
     }
 }
---
+++ EnrollmentSystem/DataStructuresLibrary/ICustomArrayList.cs
@@ -0,0 +1,12 @@
+﻿// CustomArrayList.cs
+namespace DataStructuresLibrary
+{
+    public interface ICustomArrayList<T>
+    {
+        int Count { get; }
+
+        void Add(T item);
+        T Get(int index);
+        void RemoveAt(int index);
+    }
+}
