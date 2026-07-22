a51a8dbdba57b58355531fb0ae0ccc075734e3ab
jpeustaquio <johnpaul.eustaquio@benilde.edu.ph>
2026-07-22 18:06:22 +0800

    Implemented CustomSinglyLinkedList

--- EnrollmentSystem/DataStructuresLibrary/CustomSinglyLinkedList.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomSinglyLinkedList.cs
@@ -11,7 +11,7 @@ namespace DataStructuresLibrary
         public Node(T data)
         {
             Data = data;
-            Next = null; 
+            Next = null;
         }
     }
 
@@ -21,24 +21,119 @@ namespace DataStructuresLibrary
 
         public Node<T>? Head // Mark as nullable to match the field
         {
-            get { throw new NotImplementedException(); }
+            get { return _head; }
         }
 
         public int Count { get; set; }
 
         public CustomSinglyLinkedList()
         {
-            _head = null; 
+            _head = null;
         }
 
+        // New node at the end of the list.
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newNode = new Node<T>(item);
+
+            if (_head == null)
+            {
+                _head = newNode;
+            }
+            else
+            {
+                Node<T> current = _head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+
+            Count++;
         }
 
+        // Remove the first node that matches the item and return false if not found.
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            Node<T>? previous = null;
+            Node<T>? current = _head;
+
+            while (current != null)
+            {
+                if (object.Equals(current.Data, item))
+                {
+                    if (previous == null)
+                    {
+                        _head = current.Next; // item was the first node
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                    }
+
+                    Count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        // Linear search to return the index of the item, or -1 if not found.
+        public int LinearSearch(T item)
+        {
+            Node<T>? current = _head;
+            int index = 0;
+
+            while (current != null)
+            {
+                if (object.Equals(current.Data, item))
+                {
+                    return index;
+                }
+
+                current = current.Next;
+                index++;
+            }
+
+            return -1;
+        }
+
+        // Bubble sort to keep passing through the list and swapping neighbours that are out of order until no more swaps are needed.
+        public void Sort()
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            bool swapped = true;
+
+            while (swapped)
+            {
+                swapped = false;
+                Node<T>? current = _head;
+
+                while (current != null && current.Next != null)
+                {
+                    IComparable<T> left = (IComparable<T>)current.Data!;
+
+                    if (left.CompareTo(current.Next.Data) > 0)
+                    {
+                        // swap the data of the two nodes
+                        T temp = current.Data;
+                        current.Data = current.Next.Data;
+                        current.Next.Data = temp;
+                        swapped = true;
+                    }
+
+                    current = current.Next;
+                }
+            }
         }
     }
 }
