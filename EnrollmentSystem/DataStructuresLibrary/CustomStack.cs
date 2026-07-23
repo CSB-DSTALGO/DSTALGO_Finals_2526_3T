f8d11efa6ecaa162a53ed8896dfb532d69b1f99b
enrico1283108231 <enricoluis.carpena@benilde.edu.ph>
2026-07-23 13:27:38 +0800

    Updated CustomStack

--- EnrollmentSystem/DataStructuresLibrary/CustomStack.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomStack.cs
@@ -6,36 +6,64 @@ namespace DataStructuresLibrary
     public class CustomStack<T>
     {
         private T[] _items;
-        private int _top;
+        private int _count;
 
         public int Count 
         { 
-            get { throw new NotImplementedException(); } 
+            get { return _count; } 
         }
 
         public CustomStack()
         {
-            throw new NotImplementedException();
+            _items = new T[4];
+            _count = 0;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            if (_count == _items.Length)
+            {
+                Resize();
+            }
+            _items[_count] = item;
+            _count++;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+ 
+            _count--;
+            T item = _items[_count];
+            _items[_count] = default(T)!;
+            return item;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+            }
+ 
+            return _items[_count - 1];
         }
 
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            return _count == 0;
         }
+        private void Resize()
+{
+    T[] newItems = new T[_items.Length * 2];
+    for (int i = 0; i < _count; i++)
+    {
+        newItems[i] = _items[i];
+    }
+    _items = newItems;
+}
     }
 }
