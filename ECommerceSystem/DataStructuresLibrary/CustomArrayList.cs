825e998ef4933b84dc4fc91ae89194a6eec8f4b5
Lorenzo Joaquin Picache <enzopicache0809@gmail.com>
2026-07-23 18:10:21 +0800

    Add gitignore, implement CustomArrayList and CustomSinglyLinkedList, fix target framework to net8.0

--- ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -10,14 +10,88 @@ public class CustomArrayList<T> where T : IComparable<T>
         _items = new T[initialCapacity];
     }
 
-    
-    public void Add(T item) => throw new NotImplementedException();
-    public bool Remove(T item) => throw new NotImplementedException();
-    public T Get(int index) => throw new NotImplementedException();
+    public void Add(T item)
+    {
+        // parking lot is full, need a bigger lot before we can park this one
+        if (Count == _items.Length)
+        {
+            Grow();
+        }
 
-    
-    public int Search(T item) => throw new NotImplementedException();
+        _items[Count] = item;
+        Count++;
+    }
 
-    
-    public void Sort() => throw new NotImplementedException();
+    private void Grow()
+    {
+        // just doubling the size, same idea as List<T> under the hood
+        T[] biggerLot = new T[_items.Length == 0 ? 4 : _items.Length * 2];
+
+        for (int i = 0; i < Count; i++)
+        {
+            biggerLot[i] = _items[i];
+        }
+
+        _items = biggerLot;
+    }
+
+    public bool Remove(T item)
+    {
+        int index = Search(item);
+
+        // -1 means we never found it, nothing to remove
+        if (index == -1)
+        {
+            return false;
+        }
+
+        // shift everything after the removed spot one slot to the left
+        for (int i = index; i < Count - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        Count--;
+        return true;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new IndexOutOfRangeException($"Index {index} is out of range. Count is {Count}.");
+        }
+
+        return _items[index];
+    }
+
+    public int Search(T item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (_items[i].CompareTo(item) == 0)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void Sort()
+    {
+        // basic bubble sort, fine for this ADT's scale
+        for (int i = 0; i < Count - 1; i++)
+        {
+            for (int j = 0; j < Count - 1 - i; j++)
+            {
+                if (_items[j].CompareTo(_items[j + 1]) > 0)
+                {
+                    T temp = _items[j];
+                    _items[j] = _items[j + 1];
+                    _items[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
--- ECommerceSystem/DataStructuresLibrary/CustomSinglyLinkedList.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomSinglyLinkedList.cs
@@ -12,11 +12,107 @@ public class CustomSinglyLinkedList<T> where T : IComparable<T>
     private Node? _head;
     public int Count { get; private set; }
 
-    public void Add(T item) => throw new NotImplementedException();
-    public bool Remove(T item) => throw new NotImplementedException();
+    public void Add(T item)
+    {
+        Node newNode = new Node(item);
 
-    public bool Search(T item) => throw new NotImplementedException();
+        // empty list, new node just becomes the head
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            // walk to the last node using current, never move _head itself
+            Node current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
 
-    
-    public void Sort() => throw new NotImplementedException();
+            current.Next = newNode;
+        }
+
+        Count++;
+    }
+
+    public bool Remove(T item)
+    {
+        if (_head == null)
+        {
+            return false;
+        }
+
+        // special case: the head itself is the one we want gone
+        if (_head.Data.CompareTo(item) == 0)
+        {
+            _head = _head.Next;
+            Count--;
+            return true;
+        }
+
+        // keep track of the node just behind current so we can re-link past it
+        Node current = _head;
+        while (current.Next != null)
+        {
+            if (current.Next.Data.CompareTo(item) == 0)
+            {
+                // skip over the matching node, works for middle or tail removal
+                current.Next = current.Next.Next;
+                Count--;
+                return true;
+            }
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public bool Search(T item)
+    {
+        Node? current = _head;
+
+        while (current != null)
+        {
+            if (current.Data.CompareTo(item) == 0)
+            {
+                return true;
+            }
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public void Sort()
+    {
+        // swapping the Data inside nodes is way simpler than re-wiring
+        // Next pointers, and the list still ends up in the right order
+        if (_head == null)
+        {
+            return;
+        }
+
+        bool swapped;
+        do
+        {
+            swapped = false;
+            Node? current = _head;
+
+            while (current != null && current.Next != null)
+            {
+                if (current.Data.CompareTo(current.Next.Data) > 0)
+                {
+                    T temp = current.Data;
+                    current.Data = current.Next.Data;
+                    current.Next.Data = temp;
+                    swapped = true;
+                }
+
+                current = current.Next;
+            }
+        } while (swapped);
+    }
 }
