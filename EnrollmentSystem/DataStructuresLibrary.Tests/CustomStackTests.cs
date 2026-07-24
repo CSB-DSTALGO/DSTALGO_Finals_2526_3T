c65cd8cb8ab812b53c406cd57bc9b207a10e9039
Aivan Mark M. Aguilar <aivanmark.aguilar@benilde.edu.ph>
2026-07-24 22:09:45 +0800

    Added AdministrativeLogs and CustomStackTests

--- EnrollmentSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
+++ EnrollmentSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
@@ -8,9 +8,41 @@ namespace DataStructuresLibrary.Tests
     {
         //ADD ALL YOUR TESTS HERE
         [Fact]
-        public void ThisIsYourTest()
+        public void PushTest()
         {
+            CustomStack<string> stack = new CustomStack<string>();
+            stack.Push("Log 1"); // pushes an item to the stack
 
+            Assert.Equal(1, stack.Count); // checks if the count increased
         }
-    }   
-} 
+
+        [Fact]
+        public void PopTest()
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+            stack.Push("Log 1");
+            stack.Push("Log 2");
+            stack.Pop(); // removes the most recently added item (Log 2)
+
+            Assert.Equal(1, stack.Count); // checks if the count decreased back to 1
+        }
+
+        [Fact]
+        public void PeekTest()
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+            stack.Push("Log 1");
+            stack.Push("Log 2");
+
+            Assert.Equal("Log 2", stack.Peek()); // views the top item without removing it
+        }
+
+        [Fact]
+        public void EmptyPopTest()
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+
+            Assert.Throws<InvalidOperationException>(() => stack.Pop()); // ensures popping an empty stack throws an error
+        }
+    }
+}
