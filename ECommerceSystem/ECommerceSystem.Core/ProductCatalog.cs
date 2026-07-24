d228799b995a8d7556a488332a58f037aba69585
Noeus Elna Beronio <noeuselna.beronio@benilde.edu.ph>
2026-07-25 00:57:39 +0800

    Implement generic CustomSinglyLinkedList, ProductCatalog, and unit tests

--- ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
+++ ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
@@ -8,10 +8,24 @@ public class ProductCatalog
 
     public int Count => _products.Count;
 
-    public void AddProduct(Product product) => throw new NotImplementedException();
-    public bool RemoveProduct(Product product) => throw new NotImplementedException();
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
 
-    
-    public bool SearchProduct(Product product) => throw new NotImplementedException();
-    public void SortCatalog() => throw new NotImplementedException();
+    public bool RemoveProduct(Product product)
+    {
+        return _products.Remove(product);
+    }
+
+
+    public bool SearchProduct(Product product)
+    {
+        return _products.Search(product);
+    }
+
+    public void SortCatalog()
+    {
+        _products.Sort();
+    }
 }
