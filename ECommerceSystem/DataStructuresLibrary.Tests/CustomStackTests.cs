25c0f1ba8f86d94235490bb0321617294b3f9b85
Mayn-ard <maynardvincent.arradaza@benilde.edu.ph>
2026-07-24 10:13:19 +0800

    implemented return history

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
@@ -8,28 +8,77 @@ public class CustomStackTests
     [Fact]
     public void PushAndPop_ShouldMaintainStrictLIFOOrder()
     {
-        // TODO: Test Last-In, First-Out behavior
-        throw new NotImplementedException();
+        // Arrange
+        var stack = new CustomStack<int>();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+
+        // Act
+        int first = stack.Pop();
+        int second = stack.Pop();
+        int third = stack.Pop();
+
+        // Assert
+        Assert.Equal(30, first);
+        Assert.Equal(20, second);
+        Assert.Equal(10, third);
+        Assert.Equal(0, stack.Count);
     }
 
     [Fact]
     public void Peek_ShouldReturnTopElement_WithoutRemovingIt()
     {
-        // TODO: Test Peek returning top item while keeping Count intact
-        throw new NotImplementedException();
+        // Arrange
+        var stack = new CustomStack<int>();
+
+        stack.Push(5);
+        stack.Push(15);
+
+        // Act
+        int top = stack.Peek();
+
+        // Assert
+        Assert.Equal(15, top);
+        Assert.Equal(2, stack.Count);
     }
 
     [Fact]
     public void Search_ShouldReturnOneBasedDepthFromTop_WhenItemExists()
     {
-        // TODO: Verify Search returns depth from top (1 = top item)
-        throw new NotImplementedException();
+        // Arrange
+        var stack = new CustomStack<int>();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(40);
+
+        // Act
+        int position = stack.Search(30);
+
+        // Assert
+        // Stack top: 40 (1), 30 (2), 20 (3), 10 (4)
+        Assert.Equal(2, position);
     }
 
     [Fact]
     public void Sort_ShouldReorderStack_WithSmallestItemAtTop()
     {
-        // TODO: Verify stack sorting order relative to the top reference
-        throw new NotImplementedException();
+        // Arrange
+        var stack = new CustomStack<int>();
+
+        stack.Push(30);
+        stack.Push(10);
+        stack.Push(40);
+        stack.Push(20);
+
+        // Act
+        stack.Sort();
+
+        // Assert
+        // Assuming Sort() arranges the stack so the smallest value is on top.
+        Assert.Equal(10, stack.Peek());
     }
 }
--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -2,13 +2,87 @@ namespace DataStructuresLibrary;
 
 public class CustomStack<T> where T : IComparable<T>
 {
+    private T[] _items;
+    private const int DefaultCapacity = 4;
+
     public int Count { get; private set; }
 
-    public void Push(T item) => throw new NotImplementedException();
-    public T Pop() => throw new NotImplementedException();
-    public T Peek() => throw new NotImplementedException();
+    public CustomStack()
+    {
+        _items = new T[DefaultCapacity];
+    }
 
-    public int Search(T item) => throw new NotImplementedException();
+    public void Push(T item)
+    {
+        if (Count == _items.Length)
+        {
+            Resize(_items.Length * 2);
+        }
 
-    public void Sort() => throw new NotImplementedException();
+        _items[Count] = item;
+        Count++;
+    }
+
+    public T Pop()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
+
+        Count--;
+        T item = _items[Count];
+        _items[Count] = default!;
+
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
+
+        return _items[Count - 1];
+    }
+
+    public int Search(T item)
+    {
+        int position = 1;
+
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            if (_items[i].CompareTo(item) == 0)
+                return position;
+
+            position++;
+        }
+
+        return -1;
+    }
+
+    public void Sort()
+    {
+        for (int i = 0; i < Count - 1; i++)
+        {
+            for (int j = 0; j < Count - i - 1; j++)
+            {
+                if (_items[j].CompareTo(_items[j + 1]) > 0)
+                {
+                    T temp = _items[j];
+                    _items[j] = _items[j + 1];
+                    _items[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private void Resize(int newSize)
+    {
+        T[] newArray = new T[newSize];
+
+        for (int i = 0; i < Count; i++)
+        {
+            newArray[i] = _items[i];
+        }
+
+        _items = newArray;
+    }
 }
