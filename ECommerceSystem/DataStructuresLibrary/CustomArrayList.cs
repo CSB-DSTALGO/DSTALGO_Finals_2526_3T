42c8df75bab00d80011448faf194006262ae4bf8
Precious Grace Mana-ay <ict.manaay.pg@gmail.com>
2026-07-24 18:37:16 +0800

    Implemented CustomArrayListTests and ShoppingCart and updated CustomArrayList

--- ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -9,13 +9,18 @@ public class CustomArrayList<T> where T : IComparable<T>
     public int Count { get; private set; }
 
     public CustomArrayList(int initialCapacity = 4)
-    {
+    {  
         _items = new T[initialCapacity];
         Count = 0;
     }
 
     public void Add(T item)
-    { 
+    {
+        if (Count == _items.Length)
+        {
+            Resize();
+        }
+
         _items[Count] = item;
         Count++;
     }
@@ -50,53 +55,46 @@ public class CustomArrayList<T> where T : IComparable<T>
     }
     public T Get(int index)
     { 
-        if (index < 0 || index >= _items.Length)
+        if (index < 0 || index >= Count)
         {
             throw new IndexOutOfRangeException();
         }
+
         return _items[index]; 
     }
 
-    public void Resize () //added a function for resizing of arraylist
+    public void Resize()
     {
+        int Resize = _items.Length * 2;
+        T[] newArray = new T[Resize];
 
-    }
-
-    public int Search(T item)//binary search
-    {
-        int left = 0;
-        int right = _items.Length - 1;  
-
-        while (left <= right)
+        for(int i = 0; i < Count;  i++)
         {
-            int mid = left + (right - left) / 2;
-            int compare = _items[mid].CompareTo(item);
-
-            if(compare == 0)
-            {
-                return mid;
-            }
-            else if (compare < 0)
-            {
-                left = mid + 1;
-            }
-            else
-            {
-                right = mid - 1;
-            }
+            newArray[i] = _items[i];
         }
 
-        return -1; //returns if the index is not found
+        _items = newArray;
+    }
+   
+    public int Search(T item)//linear search
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (_items[i].CompareTo(item) == 0)
+                return i;
+        }
+
+        return -1;
     }
 
     public void Sort() //insertion algorithm
     {
-        for (int i = 0; i < Count ; i++)
+        for (int i = 1; i < Count ; i++)
         {
             T key = _items[i];
             int j = i - 1;
 
-            while(j >= 0 && _items[i].CompareTo(key) > 0)
+            while(j >= 0 && _items[j].CompareTo(key) > 0)
             {
                 _items[j+1] = _items[j];
                 j = j - 1;
