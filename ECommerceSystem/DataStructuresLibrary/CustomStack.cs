5512f2db0fe3fa7eacefba9b2c82a2848611e044
Gregorio L. Dolina III <gregorio.dolinaiii@benilde.edu.ph>
2026-07-25 06:17:28 +0800

    Implement CustomStack, tests, and ReturnHistoryStack

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -2,13 +2,39 @@ namespace DataStructuresLibrary;
 
 public class CustomStack<T> where T : IComparable<T>
 {
-    public int Count { get; private set; }
+    private readonly CustomArrayList<T> _items = new();
+    public int Count => _items.Count;
 
-    public void Push(T item) => throw new NotImplementedException();
-    public T Pop() => throw new NotImplementedException();
-    public T Peek() => throw new NotImplementedException();
+    public void Push(T item)
+    {
+        _items.Add(item);
+    }
+    public T Pop()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
 
-    public int Search(T item) => throw new NotImplementedException();
+        T item = _items.Get(Count - 1);
+        _items.Remove(item);
+        return item;
+    }
+    public T Peek()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
 
-    public void Sort() => throw new NotImplementedException();
+        return _items.Get(Count - 1);
+    }
+    public int Search(T item)
+    {
+        int index = _items.Search(item);
+        if (index == -1)
+            return -1;
+
+        return Count - index;
+    }
+    public void Sort()
+    {
+        _items.Sort();
+    }
 }
--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -5,15 +5,26 @@ using DataStructuresLibrary;
 public class ReturnHistoryStack
 {
     private readonly CustomStack<ReturnRequest> _returns = new();
-
     public int Count => _returns.Count;
 
-    public void PushReturn(ReturnRequest request) => throw new NotImplementedException();
-    public ReturnRequest PopReturn() => throw new NotImplementedException();
-    public ReturnRequest PeekLatestReturn() => throw new NotImplementedException();
-
-    public bool CheckHistoryEmpty() => throw new NotImplementedException();
-    
-    public int SearchReturn(ReturnRequest request) => throw new NotImplementedException();
-    public void SortReturns() => throw new NotImplementedException();
+    public void PushReturn(ReturnRequest request)
+    {
+        _returns.Push(request);
+    }
+    public ReturnRequest PopReturn()
+    {
+        return _returns.Pop();
+    }
+    public ReturnRequest PeekLatestReturn()
+    {
+        return _returns.Peek();
+    }
+    public int SearchReturn(ReturnRequest request)
+    {
+        return _returns.Search(request);
+    }
+    public void SortReturns()
+    {
+        _returns.Sort();
+    }
 }
