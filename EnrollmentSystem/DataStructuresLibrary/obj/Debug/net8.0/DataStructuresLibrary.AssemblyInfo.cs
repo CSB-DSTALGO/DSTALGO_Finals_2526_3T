2a18d0be4790079406abdd6ff831cb6e17a90df4
Michael Ivan T. Go <michaelivango@gmail.com>
2026-07-25 00:21:16 +0800

    StudentRegistry and CustomArrayList

--- EnrollmentSystem/DataStructuresLibrary/obj/Debug/net8.0/DataStructuresLibrary.AssemblyInfo.cs
+++ EnrollmentSystem/DataStructuresLibrary/obj/Debug/net8.0/DataStructuresLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructuresLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bde3546838399bfb5f34b953b26b21a9684075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6f219338ca8738d1080c73dabbd9ac07bc7523")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructuresLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructuresLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
