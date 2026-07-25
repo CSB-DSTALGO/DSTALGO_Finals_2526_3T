87212d47e47b365cf522071bb4d24cd34a2bf21d
codewar30 <angelogabriel.ferrera@benilde.edu.ph>
2026-07-25 18:15:55 +0800

    Implement CustomArrayList methods and functionality

--- EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -1,41 +1,54 @@
-// CustomArrayList.cs
-using System;
-
 namespace DataStructuresLibrary
 {
-    public class CustomArrayList<T>
+    public class CustomArrayList
     {
         private T[] _items;
         private int _count;
+    public int Count
+    {
+        get { return _count; }
+    }
 
-        public int Count
+    public CustomArrayList()
+    {
+        _items = new T[4];
+        _count = 0;
+    }
+
+    public void Add(T item)
+    {
+        if (_count == _items.Length)
+            Resize();
+        _items[_count++] = item;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+        return _items[index];
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        // shift elements left
+        for (int i = index; i < _count - 1; i++)
         {
-            get { throw new NotImplementedException(); }
+            _items[i] = _items[i + 1];
         }
 
-        public CustomArrayList()
-        {
-            throw new NotImplementedException();
-        }
+        _count--;
+        _items[_count] = default(T); // clear reference for GC
+    }
 
-        public void Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T Get(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void Resize()
-        {
-            throw new NotImplementedException();
-        }
+    private void Resize()
+    {
+        int newSize = _items.Length == 0 ? 4 : _items.Length * 2;
+        T[] newArr = new T[newSize];
+        Array.Copy(_items, newArr, _count);
+        _items = newArr;
     }
 }
