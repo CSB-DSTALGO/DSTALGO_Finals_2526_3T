90b71b2582731e9ccdb38240e2f870e59b5fbfe2
j.san <jeremiah.santiago@benilde.edu.ph>
2026-07-25 20:16:08 +0800

    Update OrderProcessingQueue.cs

--- ECommerceSystem/ECommerceSystem.Core/OrderProcessingQueue.cs
+++ ECommerceSystem/ECommerceSystem.Core/OrderProcessingQueue.cs
@@ -2,6 +2,44 @@ namespace ECommerceSystem.Core;
 
 using DataStructuresLibrary;
 
+
+public class OrderProcessingQueue
+{
+    private readonly CustomQueue<Order> _orders = new();
+
+    public int Count => _orders.Count;
+
+ 
+    public void EnqueueOrder(Order order)
+    {
+        if (order is null)
+            throw new ArgumentNullException(nameof(order), "Cannot enqueue a null order.");
+
+        _orders.Enqueue(order);
+    }
+
+    public Order ProcessNextOrder() => _orders.Dequeue();
+
+
+    public Order PeekNextOrder() => _orders.Peek();
+
+
+    public bool CheckOrderQueueEmpty() => _orders.Count == 0;
+
+
+    public bool SearchOrder(Order order)
+    {
+        if (order is null) return false;
+        return _orders.Search(order);
+    }
+
+
+    public void SortOrders() => _orders.Sort();
+}
+namespace ECommerceSystem.Core;
+
+using DataStructuresLibrary;
+
 public class OrderProcessingQueue
 {
     private readonly CustomQueue<Order> _orders = new();
