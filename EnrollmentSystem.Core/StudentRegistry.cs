7b0af1bfdb5b4b51fc04668bbc2c01266a2bb375
onlyJAP <jgpetrola@gmail.com>
2026-07-25 22:47:40 +0800

    copied member 1's student registry manually because pulling wont reflect

--- EnrollmentSystem.Core/StudentRegistry.cs
+++ EnrollmentSystem.Core/StudentRegistry.cs
@@ -1,36 +1,97 @@
-// StudentRegistry.cs
-using System;
+namespace EnrollmentSystem.Core;
+
 using DataStructuresLibrary;
 
-namespace EnrollmentSystem.Core
+public class StudentRegistry
 {
-    public class StudentRegistry
+    private readonly CustomArrayList<Student> _students = new();
+
+    public int Count => _students.Count;
+
+    public void RegisterStudent(Student student)
     {
-        private readonly CustomArrayList<Student> _registry;
+        _students.Add(student);
+    }
 
-        public StudentRegistry()
-        {
-            _registry = new CustomArrayList<Student>();
-        }
+    public bool UnregisterStudent(int index)
+    {
+        if (index < 0 || index >= _students.Count)
+            return false;
 
-        public void RegisterStudent(Student student)
-        {
-            throw new NotImplementedException();
-        }
+        _students.RemoveAt(index);
+        return true;
+    }
 
-        public void UnregisterStudent(int index)
+    public bool RemoveStudent(string id)
+    {
+        for (int i = 0; i < _students.Count; i++)
         {
-            throw new NotImplementedException();
+            if (_students.Get(i).Id == id)
+            {
+                _students.RemoveAt(i);
+                return true;
+            }
         }
+        return false;
+    }
 
-        public Student GetStudentDetails(int index)
-        {
-            throw new NotImplementedException();
-        }
+    public Student GetStudentAt(int index)
+    {
+        return _students.Get(index);
+    }
 
-        public void ShowAllStudents()
+    public Student GetStudentDetails(int index)
+    {
+        return _students.Get(index);
+    }
+
+    public void ShowAllStudents()
+    {
+        for (int i = 0; i < _students.Count; i++)
         {
-            throw new NotImplementedException();
+            var s = _students.Get(i);
+            Console.WriteLine($"[{i}] ID: {s.Id} | Name: {s.Name} | Course: {s.CourseCode}");
         }
+    }
+
+    public int SearchStudent(Student student)
+    {
+        for (int i = 0; i < _students.Count; i++)
+        {
+            if (_students.Get(i).Id == student.Id)
+                return i;
+        }
+        return -1;
+    }
+
+    // SORTING ALGORITHM: Bubble Sort.
+    // Sorts students alphabetically by Id.
+    // Time Complexity: O(n^2) worst case, O(n) best case (with early exit).
+    public void SortStudentsById()
+    {
+        int n = _students.Count;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                var a = _students.Get(j);
+                var b = _students.Get(j + 1);
+
+                if (string.Compare(a.Id, b.Id) > 0)
+                {
+                    _students.Set(j, b);
+                    _students.Set(j + 1, a);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public int GetStudentCount()
+    {
+        return _students.Count;
     }
 }
