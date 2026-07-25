fe8f807645730d8d88874b51b9fda533163be940
Galiano <johngaliano.valenzuela@benilde.edu.ph>
2026-07-25 22:01:24 +0800

    Implement CustomStack and AdministrativeLogs

--- EnrollmentSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
+++ EnrollmentSystem/DataStructuresLibrary.Tests/CustomStackTests.cs
@@ -8,9 +8,71 @@ namespace DataStructuresLibrary.Tests
     {
         //ADD ALL YOUR TESTS HERE
         [Fact]
-        public void ThisIsYourTest()
+        public void NewStack_ShouldBeEmpty()
         {
+            var stack = new CustomStack<int>();
 
+            Assert.True(stack.IsEmpty());
+            Assert.Equal(0, stack.Count);
         }
-    }   
-} 
+
+        [Fact]
+        public void Push_ShouldIncreaseCount()
+        {
+            var stack = new CustomStack<int>();
+
+            stack.Push(10);
+
+            Assert.Equal(1, stack.Count);
+        }
+
+        [Fact]
+        public void Push_And_Peek_ShouldReturnLastItem()
+        {
+            var stack = new CustomStack<string>();
+
+            stack.Push("A");
+            stack.Push("B");
+
+            Assert.Equal("B", stack.Peek());
+        }
+
+        [Fact]
+        public void Pop_ShouldReturnLastItem()
+        {
+            var stack = new CustomStack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+
+            Assert.Equal(2, stack.Pop());
+            Assert.Equal(1, stack.Count);
+        }
+
+        [Fact]
+        public void Pop_OnEmptyStack_ShouldThrow()
+        {
+            var stack = new CustomStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Fact]
+        public void Peek_OnEmptyStack_ShouldThrow()
+        {
+            var stack = new CustomStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+
+        [Fact]
+        public void IsEmpty_ShouldReturnFalseAfterPush()
+        {
+            var stack = new CustomStack<int>();
+
+            stack.Push(100);
+
+            Assert.False(stack.IsEmpty());
+        }
+    }
+}
