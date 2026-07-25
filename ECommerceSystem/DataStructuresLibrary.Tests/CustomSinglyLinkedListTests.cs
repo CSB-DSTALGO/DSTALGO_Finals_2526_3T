689a19452a428d8f8be2da0d1e405a21a2f594b3
Lance Adrian Espiritu <LanceAdrian.Espiritu@benilde.edu.ph>
2026-07-25 09:49:09 +0800

    Describe your changes

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomSinglyLinkedListTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomSinglyLinkedListTests.cs
@@ -9,34 +9,78 @@ public class CustomSinglyLinkedListTests
     public void Add_ShouldAppendNodeAndIncrementCount()
     {
         // TODO: Test appending items to the linked list
-        throw new NotImplementedException();
+        var list = new CustomSinglyLinkedList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        Assert.Equal(3, list.Count);
+        Assert.True(list.Search(10));
+        Assert.True(list.Search(20));
+        Assert.True(list.Search(30));
+
     }
 
     [Fact]
     public void Remove_ShouldUpdateNodePointersCorrectly()
     {
         // TODO: Test removing head, middle, and tail nodes
-        throw new NotImplementedException();
+        var list = new CustomSinglyLinkedList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        Assert.True(list.Remove(20));
+        Assert.Equal(2, list.Count);
+        Assert.False(list.Search(20));
     }
 
     [Fact]
     public void Search_ShouldReturnTrue_WhenItemExistsInNodes()
     {
         // TODO: Test linear node traversal finding existing data
-        throw new NotImplementedException();
+        var list = new CustomSinglyLinkedList<int>();
+
+        list.Add(5);
+        list.Add(10);
+        list.Add(15);
+
+        Assert.True(list.Search(10));
     }
 
     [Fact]
     public void Search_ShouldReturnFalse_WhenItemIsAbsent()
     {
         // TODO: Test linear search returning false for missing data
-        throw new NotImplementedException();
+        var list = new CustomSinglyLinkedList<int>();
+
+        list.Add(5);
+        list.Add(10);
+        list.Add(15);
+
+        Assert.False(list.Search(20));
     }
 
     [Fact]
     public void Sort_ShouldRearrangeNodePointersInAscendingOrder()
     {
         // TODO: Test node re-linking to verify ascending list order
-        throw new NotImplementedException();
+        var list = new CustomSinglyLinkedList<int>();
+
+        list.Add(3);
+        list.Add(1);
+        list.Add(4);
+        list.Add(2);
+
+        list.Sort();
+
+        Assert.Equal(4, list.Count);
+        Assert.True(list.Search(1));
+        Assert.True(list.Search(2));
+        Assert.True(list.Search(3));
+        Assert.True(list.Search(4));
+
     }
 }
