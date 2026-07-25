1024a4b6257ac61274a39968d84d2314230cf680
j.san <jeremiah.santiago@benilde.edu.ph>
2026-07-25 20:17:03 +0800

    Update ReturnHistoryStack.cs

--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -8,6 +8,40 @@ public class ReturnHistoryStack
 
     public int Count => _returns.Count;
 
+
+    public void PushReturn(ReturnRequest request)
+    {
+        if (request is null)
+            throw new ArgumentNullException(nameof(request), "Cannot push a null return request.");
+
+        _returns.Push(request);
+    }
+
+    public ReturnRequest PopReturn() => _returns.Pop();
+
+    public ReturnRequest PeekLatestReturn() => _returns.Peek();
+
+    public bool CheckHistoryEmpty() => _returns.Count == 0;
+
+    public int SearchReturn(ReturnRequest request)
+    {
+        if (request is null) return -1;
+        return _returns.Search(request);
+    }
+
+  
+    public void SortReturns() => _returns.Sort();
+}
+namespace ECommerceSystem.Core;
+
+using DataStructuresLibrary;
+
+public class ReturnHistoryStack
+{
+    private readonly CustomStack<ReturnRequest> _returns = new();
+
+    public int Count => _returns.Count;
+
     public void PushReturn(ReturnRequest request) => throw new NotImplementedException();
     public ReturnRequest PopReturn() => throw new NotImplementedException();
     public ReturnRequest PeekLatestReturn() => throw new NotImplementedException();
