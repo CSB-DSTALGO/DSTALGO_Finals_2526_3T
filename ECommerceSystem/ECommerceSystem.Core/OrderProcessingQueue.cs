a0fe0bb551704f92e13b1e82707d3a7278c6a066
user <user@JEHJEH-3103>
2026-07-25 19:40:06 +0800

    Implemented OrderProcessing Queue

--- ECommerceSystem/ECommerceSystem.Core/OrderProcessingQueue.cs
+++ ECommerceSystem/ECommerceSystem.Core/OrderProcessingQueue.cs
@@ -5,14 +5,40 @@ using DataStructuresLibrary;
 public class OrderProcessingQueue
 {
     private readonly CustomQueue<Order> _orders = new();
-
     public int Count => _orders.Count;
 
-    public void EnqueueOrder(Order order) => throw new NotImplementedException();
-    public Order ProcessNextOrder() => throw new NotImplementedException();
-    public Order PeekNextOrder() => throw new NotImplementedException();
+    //Enqueues an order to the back
+    public void EnqueueOrder(Order order)
+    {
+        if (order is null)
+        {
+            throw new ArgumentNullException(nameof(order));
+        }
+        _orders.Enqueue(order);
+    }
 
-    
-    public bool SearchOrder(Order order) => throw new NotImplementedException();
-    public void SortOrders() => throw new NotImplementedException();
+    //Dequeues and returns the front order
+    public Order ProcessNextOrder()
+    {
+        return _orders.Dequeue();
+    }
+
+    //Returns the front order without removing it
+    public Order PeekNextOrder()
+    {
+        return _orders.Peek();
+    }
+
+    //Checks if a matching order exists in the queue
+    //if it successfully searches for an existing order it returns it
+    public bool SearchOrder(Order order)
+    {
+        return _orders.Search(order);
+    }
+
+    //Sorts the queues contents using CustomQueue bubble sort
+    public void SortOrders()
+    {
+        _orders.Sort();
+    }
 }
