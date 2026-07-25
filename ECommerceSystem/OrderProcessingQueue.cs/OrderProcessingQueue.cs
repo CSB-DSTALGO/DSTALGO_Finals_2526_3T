45ca77af68e8426afd3a1fd93d053cbd37f2b46c
RJBarba <raymondjosef.barba@benilde.edu.ph>
2026-07-26 07:39:35 +0800

    Add files via upload
    Implement CustomQueue, OrderProcessingQueue, and Unit Tests

---
+++ ECommerceSystem/OrderProcessingQueue.cs/OrderProcessingQueue.cs
@@ -0,0 +1,17 @@
+namespace ECommerceSystem.Core;
+
+using DataStructuresLibrary;
+
+public class OrderProcessingQueue
+{
+    private readonly CustomQueue<Order> _orders = new();
+
+    public int Count => _orders.Count;
+
+    public void EnqueueOrder(Order order) => _orders.Enqueue(order);
+    public Order ProcessNextOrder() => _orders.Dequeue();
+    public Order PeekNextOrder() => _orders.Peek();
+
+    public bool SearchOrder(Order order) => _orders.Contains(order);
+    public void SortOrders() => _orders.Sort();
+}
