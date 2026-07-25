bc99c055d1947546448867b7e3463c5befe1ecc1
Tristen Dugenia <tristen.dugenia@benilde.edu.ph>
2026-07-25 17:13:16 +0800

    Revert "Implement CustomStack (push, pop, peek, search, sort) and ReturnHistoryStack with unit tests"
    This reverts commit 1f01e7fd36689143f49abcd8d7014d4e02db21a8.

--- EnrollmentSystem/DataStructuresLibrary/CustomStack.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomStack.cs
@@ -1,62 +1,41 @@
 // CustomStack.cs
 using System;
+
 namespace DataStructuresLibrary
 {
     public class CustomStack<T>
     {
         private T[] _items;
-        private int _top; // number of items currently in the stack
+        private int _top;
 
-        public int Count
-        {
-            get { return _top; }
+        public int Count 
+        { 
+            get { throw new NotImplementedException(); } 
         }
 
         public CustomStack()
         {
-            _items = new T[4]; // starting capacity
-            _top = 0;
+            throw new NotImplementedException();
         }
 
-        // Pushes an item onto the top of the stack
         public void Push(T item)
         {
-            if (_top == _items.Length)
-                Resize();
-            _items[_top] = item;
-            _top++;
+            throw new NotImplementedException();
         }
 
-        // Removes and returns the top item
         public T Pop()
         {
-            if (IsEmpty())
-                throw new InvalidOperationException("Stack is empty.");
-            _top--;
-            return _items[_top];
+            throw new NotImplementedException();
         }
 
-        // Returns the top item without removing it
         public T Peek()
         {
-            if (IsEmpty())
-                throw new InvalidOperationException("Stack is empty.");
-            return _items[_top - 1];
+            throw new NotImplementedException();
         }
 
-        // Returns true if the stack has no items
         public bool IsEmpty()
         {
-            return _top == 0;
-        }
-
-        // Doubles the internal array size when full
-        private void Resize()
-        {
-            T[] bigger = new T[_items.Length * 2];
-            for (int i = 0; i < _top; i++)
-                bigger[i] = _items[i];
-            _items = bigger;
+            throw new NotImplementedException();
         }
     }
 }
