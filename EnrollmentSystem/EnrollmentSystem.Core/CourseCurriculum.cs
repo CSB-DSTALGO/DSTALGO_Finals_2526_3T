4915044143f8c272a9eda818ab48314624dd7fa5
Tristan Cerlavo <tristankristofer.ceralvo@benilde.edu.ph>
2026-07-25 15:01:43 +0800

    Filled CustomSinglyLinkedList & CourseCurriculum, CustomSinglyLinkedList in progress of being worked on

--- EnrollmentSystem/EnrollmentSystem.Core/CourseCurriculum.cs
+++ EnrollmentSystem/EnrollmentSystem.Core/CourseCurriculum.cs
@@ -8,14 +8,42 @@ public class CourseCurriculum
 
     public int Count => _courses.Count;
 
-    public void InsertCourse(Course course) => throw new NotImplementedException();
-    public bool DeleteCourse(string code) => throw new NotImplementedException();
-
+    public void InsertCourse(Course course)
+    {
+        // Appends to the last section of the list
+        _courses.AddLast(course);
+    }
+    public bool DeleteCourse(string code)
+    {
+        // intended to use Remove method to remove specified course
+        _courses.Remove(code);
+    }
     // Hint: Sum total credit units across all courses
-    public int CalculateTotalUnits() => throw new NotImplementedException();
-    public void ShowCurriculum() => throw new NotImplementedException(); 
+    public int CalculateTotalUnits()
+    {
+        // todo later unable to determine value of 'Courses' to display Credit Units
+        throw new NotImplementedException();
+    }
+    public void ShowCurriculum()
+    {
+        // intended to show each node in the course list
+        Console.WriteLine("CURRENT COURSES:");
+        foreach (string str in _courses)
+        {
+            Console.WriteLine(str);
+        }
+    }
 
     // Hint: Delegate search and sort to CustomSinglyLinkedList<T>
-    public bool SearchCourse(Course course) => throw new NotImplementedException();
-    public void SortCurriculumByUnits() => throw new NotImplementedException();
+    public bool SearchCourse(Course course)
+    {
+        // PROBLEMATIC !! This Code might not work as intended !! PROBLEMATIC
+        Node Search = CustomSinglyLinkedList(course);
+        Console.WriteLine(Search);
+    }
+    public void SortCurriculumByUnits()
+    {
+        // todo after 'Calculate Total Units', need to determine value of 'Courses' & Credit Unit to implement
+        throw new NotImplementedException();
+    }
 }
