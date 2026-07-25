9a5965ddd1db41535ad2d464f88379595fbf5679
RJTN <renklerjoshua.norona@benilde.edu.ph>
2026-07-25 16:29:02 +0800

    CustomSinglyLinkedList

--- EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
+++ EnrollmentSystem/DataStructuresLibrary/CustomArrayList.cs
@@ -1,41 +1,103 @@
-// CustomArrayList.cs
 using System;
 
 namespace DataStructuresLibrary
 {
-    public class CustomArrayList<T>
+    public class Node<T>
     {
-        private T[] _items;
-        private int _count;
+        public T Data { get; set; }
+        public Node<T>? Next { get; set; }
 
-        public int Count 
-        { 
-            get { throw new NotImplementedException(); } 
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class CustomSinglyLinkedList<T>
+    {
+        private Node<T>? _head;
+
+        public Node<T>? Head
+        {
+            get { return _head; }
         }
 
-        public CustomArrayList()
+        public int Count { get; set; }
+
+        public CustomSinglyLinkedList()
         {
-            throw new NotImplementedException();
+            _head = null;
+            Count = 0;
         }
 
-        public void Add(T item)
+        public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node<T> newNode = new Node<T>(item);
+
+            if (_head == null)
+            {
+                _head = newNode;
+            }
+            else
+            {
+                Node<T> current = _head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            Count++;
         }
 
-        public T Get(int index)
+        public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            if (_head == null) return false;
+
+            if (_head.Data != null && _head.Data.Equals(item))
+            {
+                _head = _head.Next;
+                Count--;
+                return true;
+            }
+
+            Node<T> current = _head;
+            while (current.Next != null)
+            {
+                if (current.Next.Data != null && current.Next.Data.Equals(item))
+                {
+                    current.Next = current.Next.Next;
+                    Count--;
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
         }
 
-        public void RemoveAt(int index)
+        public T GetAt(int index)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index >= Count)
+                throw new IndexOutOfRangeException("Index out of bounds");
+
+            Node<T> current = _head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.Data;
         }
 
-        private void Resize()
+        public void ShowAll()
         {
-            throw new NotImplementedException();
+            Node<T> current = _head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data?.ToString());
+                current = current.Next;
+            }
         }
     }
 }
