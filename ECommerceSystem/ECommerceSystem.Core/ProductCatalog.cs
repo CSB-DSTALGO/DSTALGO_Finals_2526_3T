cb6969dd3538a3176ab2d9332a00080be977f18e
j.san <jeremiah.santiago@benilde.edu.ph>
2026-07-25 20:15:26 +0800

    Update ProductCatalog.cs

--- ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
+++ ECommerceSystem/ECommerceSystem.Core/ProductCatalog.cs
@@ -2,16 +2,65 @@ namespace ECommerceSystem.Core;
 
 using DataStructuresLibrary;
 
+
 public class ProductCatalog
 {
     private readonly CustomSinglyLinkedList<Product> _products = new();
 
     public int Count => _products.Count;
 
-    public void AddProduct(Product product) => throw new NotImplementedException();
-    public bool RemoveProduct(Product product) => throw new NotImplementedException();
+    public void AddProduct(Product product)
+    {
+        if (product is null)
+            throw new ArgumentNullException(nameof(product), "Cannot add a null product.");
 
-    
-    public bool SearchProduct(Product product) => throw new NotImplementedException();
-    public void SortCatalog() => throw new NotImplementedException();
+        _products.Add(product);
+    }
+
+
+    public bool RemoveProduct(Product product)
+    {
+        if (product is null) return false;
+        return _products.Remove(product);
+    }
+
+    public bool SearchProduct(Product product)
+    {
+        if (product is null) return false;
+        return _products.Search(product);
+    }
+
+    public void SortCatalog() => _products.Sort();
+
+    public Product GetProductDetails(int index)
+    {
+        if (index < 0 || index >= _products.Count)
+            throw new ArgumentOutOfRangeException(nameof(index), "No product exists at that position.");
+
+        int i = 0;
+        foreach (var product in _products.GetAll())
+        {
+            if (i == index) return product;
+            i++;
+        }
+
+        throw new ArgumentOutOfRangeException(nameof(index));
+    }
+
+    public void ShowAllProfiles()
+    {
+        if (_products.Count == 0)
+        {
+            Console.WriteLine("The catalog is empty.");
+            return;
+        }
+
+        Console.WriteLine("=== Product Catalog ===");
+        int i = 0;
+        foreach (var product in _products.GetAll())
+        {
+            Console.WriteLine($"[{i}] {product.Name} - ${product.Price:F2}");
+            i++;
+        }
+    }
 }
