a612d5e9ba7005f1e6cf3e0454fbc7c55e027fb1
Tatami_x64 <mateojpcastaneda@gmail.com>
2026-07-25 15:08:15 +0800

    Implement CustomQueue with sort and search, wire up OrderProcessingQueue, add unit tests

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomQueueTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomQueueTests.cs
@@ -8,28 +8,55 @@ public class CustomQueueTests
     [Fact]
     public void EnqueueAndDequeue_ShouldMaintainStrictFIFOOrder()
     {
-        // TODO: Test First-In, First-Out behavior
-        throw new NotImplementedException();
+        var queue = new CustomQueue<int>();
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+
+        Assert.Equal(10, queue.Dequeue());
+        Assert.Equal(20, queue.Dequeue());
+        Assert.Equal(30, queue.Dequeue());
+        Assert.Equal(0, queue.Count);
     }
 
     [Fact]
     public void Peek_ShouldReturnFrontElement_WithoutRemovingIt()
     {
-        // TODO: Test Peek maintaining Count and queue head state
-        throw new NotImplementedException();
+        var queue = new CustomQueue<int>();
+        queue.Enqueue(5);
+        queue.Enqueue(15);
+
+        Assert.Equal(5, queue.Peek());
+        Assert.Equal(2, queue.Count);
     }
 
     [Fact]
     public void Search_ShouldFindElement_WithoutAlteringQueueOrder()
     {
-        // TODO: Verify Search finds item and leaves queue order intact
-        throw new NotImplementedException();
+        var queue = new CustomQueue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        bool found = queue.Search(2);
+
+        Assert.True(found);
+        Assert.Equal(3, queue.Count);
+        Assert.Equal(1, queue.Peek());
     }
 
     [Fact]
     public void Sort_ShouldReorderQueueElementsInAscendingSequence()
     {
-        // TODO: Test sorting elements inside the FIFO queue
-        throw new NotImplementedException();
+        var queue = new CustomQueue<int>();
+        queue.Enqueue(30);
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+
+        queue.Sort();
+
+        Assert.Equal(10, queue.Dequeue());
+        Assert.Equal(20, queue.Dequeue());
+        Assert.Equal(30, queue.Dequeue());
     }
 }
--- ECommerceSystem/DataStructuresLibrary/CustomQueue.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomQueue.cs
@@ -2,13 +2,98 @@ namespace DataStructuresLibrary;
 
 public class CustomQueue<T> where T : IComparable<T>
 {
+    private T[] _items;
+    private int _front;
+    private int _rear;
+
     public int Count { get; private set; }
 
-    public void Enqueue(T item) => throw new NotImplementedException();
-    public T Dequeue() => throw new NotImplementedException();
-    public T Peek() => throw new NotImplementedException();
+    public CustomQueue(int initialCapacity = 4)
+    {
+        _items = new T[initialCapacity];
+        _front = 0;
+        _rear = -1;
+        Count = 0;
+    }
 
-    public bool Search(T item) => throw new NotImplementedException();
+    public void Enqueue(T item)
+    {
+        if (Count == _items.Length)
+        {
+            Resize();
+        }
+        _rear = (_rear + 1) % _items.Length;
+        _items[_rear] = item;
+        Count++;
+    }
 
-    public void Sort() => throw new NotImplementedException();
+    public T Dequeue()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Queue is empty.");
+
+        T item = _items[_front];
+        _front = (_front + 1) % _items.Length;
+        Count--;
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Queue is empty.");
+
+        return _items[_front];
+    }
+
+    public bool Search(T item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            int index = (_front + i) % _items.Length;
+            if (_items[index].CompareTo(item) == 0)
+                return true;
+        }
+        return false;
+    }
+
+    public void Sort()
+    {
+        T[] temp = new T[Count];
+        for (int i = 0; i < Count; i++)
+        {
+            temp[i] = _items[(_front + i) % _items.Length];
+        }
+
+        for (int i = 1; i < temp.Length; i++)
+        {
+            T key = temp[i];
+            int j = i - 1;
+            while (j >= 0 && temp[j].CompareTo(key) > 0)
+            {
+                temp[j + 1] = temp[j];
+                j--;
+            }
+            temp[j + 1] = key;
+        }
+
+        _front = 0;
+        _rear = temp.Length - 1;
+        for (int i = 0; i < temp.Length; i++)
+        {
+            _items[i] = temp[i];
+        }
+    }
+
+    private void Resize()
+    {
+        T[] newItems = new T[_items.Length * 2];
+        for (int i = 0; i < Count; i++)
+        {
+            newItems[i] = _items[(_front + i) % _items.Length];
+        }
+        _items = newItems;
+        _front = 0;
+        _rear = Count - 1;
+    }
 }
