aa8b53c5b85e930f2c0095a0bc254e457548faf0
Ayeisha Hautea <ayeishalyonna.hautea@benilde.edu.ph>
2026-07-25 11:20:49 +0800

    Implement CustomStack, ReturnHistoryStack, and CustomStackTests

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -2,13 +2,87 @@ namespace DataStructuresLibrary;
 
 public class CustomStack<T> where T : IComparable<T>
 {
+    private T[] _items;
+
     public int Count { get; private set; }
 
-    public void Push(T item) => throw new NotImplementedException();
-    public T Pop() => throw new NotImplementedException();
-    public T Peek() => throw new NotImplementedException();
+    public CustomStack(int initialCapacity = 4)
+    {
+        _items = new T[initialCapacity];
+    }
 
-    public int Search(T item) => throw new NotImplementedException();
+    private void Resize()
+    {
+        T[] newItems = new T[_items.Length * 2];
 
-    public void Sort() => throw new NotImplementedException();
+        for (int i = 0; i < Count; i++)
+        {
+            newItems[i] = _items[i];
+        }
+
+        _items = newItems;
+    }
+
+    public void Push(T item)
+    {
+        if (Count == _items.Length)
+        {
+            Resize();
+        }
+
+        _items[Count] = item;
+        Count++;
+    }
+
+    public T Pop()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
+
+        Count--;
+        T item = _items[Count];
+        _items[Count] = default!;
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
+
+        return _items[Count - 1];
+    }
+
+    public int Search(T item)
+    {
+        int depth = 1;
+
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            if (_items[i].CompareTo(item) == 0)
+            {
+                return depth;
+            }
+
+            depth++;
+        }
+
+        return -1;
+    }
+
+    public void Sort()
+    {
+        for (int i = 0; i < Count - 1; i++)
+        {
+            for (int j = i + 1; j < Count; j++)
+            {
+                if (_items[i].CompareTo(_items[j]) < 0)
+                {
+                    T temp = _items[i];
+                    _items[i] = _items[j];
+                    _items[j] = temp;
+                }
+            }
+        }
+    }
 }
