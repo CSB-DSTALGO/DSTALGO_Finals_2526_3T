f5baf5c056330c1e3f7eeceabee56fc466c29dc5
codewar30 <angelogabriel.ferrera@benilde.edu.ph>
2026-07-25 18:21:22 +0800

    Implement student registration and management methods

--- EnrollmentSystem/EnrollmentSystem.Core/StudentRegistry.cs
+++ EnrollmentSystem/EnrollmentSystem.Core/StudentRegistry.cs
@@ -8,16 +8,84 @@ public class StudentRegistry
 
     public int Count => _students.Count;
 
-    public void RegisterStudent(Student student) => throw new NotImplementedException();
-    public bool UnregisterStudent(int index) => throw new NotImplementedException();
-    public bool RemoveStudent(string id) => throw new NotImplementedException();
-    public Student GetStudentAt(int index) => throw new NotImplementedException();
+    public void RegisterStudent(Student student)
+    {
+        ArgumentNullException.ThrowIfNull(student);
+        _students.Add(student);
+    }
 
-    // Hint: Calculate average GPA of all registered students
-    public double CalculateAverageGpa() => throw new NotImplementedException();
+    public bool UnregisterStudent(int index)
+    {
+        if (index < 0 || index >= _students.Count)
+        {
+            return false;
+        }
 
-    // Hint: Delegate search and sort to CustomArrayList<T>
-    public int SearchStudent(Student student) => throw new NotImplementedException();
-    public void SortStudentsByGpa() => throw new NotImplementedException();
-    public int GetStudentCount() => throw new NotImplementedException();
+        _students.RemoveAt(index);
+        return true;
+    }
+
+    public bool RemoveStudent(string id)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+        {
+            return false;
+        }
+
+        for (int i = 0; i < _students.Count; i++)
+        {
+            if (_students[i]?.Id == id)
+            {
+                _students.RemoveAt(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Student GetStudentAt(int index)
+    {
+        if (index < 0 || index >= _students.Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
+        }
+
+        return _students[index];
+    }
+
+    // Calculate average GPA of all registered students
+    public double CalculateAverageGpa()
+    {
+        if (_students.Count == 0)
+        {
+            return 0.0;
+        }
+
+        double totalGpa = 0;
+        for (int i = 0; i < _students.Count; i++)
+        {
+            totalGpa += _students[i].Gpa;
+        }
+
+        return totalGpa / _students.Count;
+    }
+
+    // Delegate search and sort to CustomArrayList<T>
+    public int SearchStudent(Student student)
+    {
+        return _students.IndexOf(student);
+    }
+
+    public void SortStudentsByGpa()
+    {
+        // Sorts descending/ascending depending on how your CustomArrayList.Sort or IComparable is set up.
+        // If CustomArrayList accepts a Comparison or IComparer:
+        _students.Sort((a, b) => b.Gpa.CompareTo(a.Gpa));
+    }
+
+    public int GetStudentCount()
+    {
+        return _students.Count;
+    }
 }
