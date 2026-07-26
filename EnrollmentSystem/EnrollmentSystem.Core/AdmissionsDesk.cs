e1c8fd39744931d6b97e7172c3bdc40cfa307118
Alexandra Tuazon <alexmtuazon2006@gmail.com>
2026-07-26 18:50:48 +0800

    updated admissiondesk.cs

--- EnrollmentSystem/EnrollmentSystem.Core/AdmissionsDesk.cs
+++ EnrollmentSystem/EnrollmentSystem.Core/AdmissionsDesk.cs
@@ -5,19 +5,26 @@ using DataStructuresLibrary;
 public class AdmissionsDesk
 {
     private readonly CustomQueue<AdmissionApplication> _applications = new();
+    private readonly CustomQueue<Ticket> _tickets = new();
 
-    public int Count => _applications.Count;
+    public int Count => _tickets.Count + _applications.Count;
 
     public void IssueAdmissionsTicket(AdmissionApplication app)
     {
         _applications.Enqueue(app); //adds the application sa queueing
     }
-    public void IssueAdmissionsTicket(Ticket ticket) => throw new NotImplementedException();
+    public void IssueAdmissionsTicket(Ticket ticket)
+    {
+        _tickets.Enqueue(ticket); //adds the ticket sa queueing
+    }
     public AdmissionApplication ServeNextStudent()
     {
        return _applications.Dequeue(); //removes the next student sa queue line
     }
-    public Ticket ServeNextTicket() => throw new NotImplementedException();
+    public Ticket ServeNextTicket()
+    {
+        return _tickets.Dequeue();
+    }
     public AdmissionApplication ViewNextTicket()
     {
         return _applications.Peek(); //returns next student application nang di nireremove sa queue line
