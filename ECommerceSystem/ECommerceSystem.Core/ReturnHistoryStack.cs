ddab2e4ba87123be2557320f2dacd2e9eee08da9
Billy B. Caling Jr. <billy.calingjr@benilde.edu.ph>
2026-07-27 19:25:30 +0800

    Fixed Format of ReturnHistoryStack

--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -8,25 +8,28 @@ public class ReturnHistoryStack
 
     public int Count => _returns.Count;
 
-   public void PushReturn(ReturnRequest request)
-{
-    _returns.Push(request);
-}
+    public void PushReturn(ReturnRequest request)
+    {
+        _returns.Push(request);
+    }
+
     public ReturnRequest PopReturn()
-{
-    return _returns.Pop();
-}
+    {
+        return _returns.Pop();
+    }
+
     public ReturnRequest PeekLatestReturn()
-{
-    return _returns.Peek();
-}
+    {
+        return _returns.Peek();
+    }
 
     public int SearchReturn(ReturnRequest request)
-{
-    return _returns.Search(request);
-}
-   public void SortReturns()
-{
-    _returns.Sort();
-}
+    {
+        return _returns.Search(request);
+    }
+
+    public void SortReturns()
+    {
+        _returns.Sort();
+    }
 }
