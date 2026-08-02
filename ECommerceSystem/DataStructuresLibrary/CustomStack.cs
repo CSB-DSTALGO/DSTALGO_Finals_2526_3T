1448ebad75329781c95323935fba5ff269568140
Gregorio L. Dolina III <gregorio.dolinaiii@benilde.edu.ph>
2026-08-02 23:15:49 +0800

    Update CustomStack & returnhistorystack

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -8,11 +8,13 @@ public class CustomStack<T> where T : IComparable<T>
 
     public void Push(T item)
     {
+        //Adds a new element to the top of the stack
         _items.Add(item);
     }
-
+   
     public T Pop()
     {
+        //Remves an element from the top of the stack
         if (Count == 0)
             throw new InvalidOperationException("Stack is empty.");
 
@@ -26,14 +28,17 @@ public class CustomStack<T> where T : IComparable<T>
 
     public T Peek()
     {
+        //Returns the element at the top of the stack without removing it
         if (Count == 0)
             throw new InvalidOperationException("Stack is empty.");
 
         return _items.Get(Count - 1);
     }
 
+    
     public int Search(T item)
     {
+        //Searches for an element
         int index = _items.Search(item);
 
         if (index == -1)
@@ -44,6 +49,23 @@ public class CustomStack<T> where T : IComparable<T>
 
     public void Sort()
     {
-        _items.Sort();
+        // Reverse the order so the smallest value becomes the top of the stack.
+        _items.Sort();    
+        CustomArrayList<T> temp = new();
+
+        for (int i = Count - 1; i >= 0; i--)
+        {
+            temp.Add(_items.Get(i));
+        }
+
+        while (Count > 0)
+        {
+            _items.Remove(_items.Get(0));
+        }
+
+        for (int i = 0; i < temp.Count; i++)
+        {
+            _items.Add(temp.Get(i));
+        }
     }
 }
--- ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
+++ ECommerceSystem/ECommerceSystem.Core/ReturnHistoryStack.cs
@@ -9,22 +9,27 @@ public class ReturnHistoryStack
 
     public void PushReturn(ReturnRequest request)
     {
+        //Adds a new return request to the top of the history
         _returns.Push(request);
     }
     public ReturnRequest PopReturn()
     {
+        //Removes the most recent return request
         return _returns.Pop();
     }
     public ReturnRequest PeekLatestReturn()
     {
+        //Views the most recent return request without removing it
         return _returns.Peek();
     }
     public int SearchReturn(ReturnRequest request)
     {
+        //Searches for a specific return request
         return _returns.Search(request);
     }
     public void SortReturns()
     {
+        //Sorts the return requests
         _returns.Sort();
     }
 }
