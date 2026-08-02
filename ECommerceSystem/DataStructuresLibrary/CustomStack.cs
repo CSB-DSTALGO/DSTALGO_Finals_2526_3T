cb2d79fbf19b1084c4b7ebc727ce104dffcea82a
Gregorio L. Dolina III <gregorio.dolinaiii@benilde.edu.ph>
2026-08-02 22:07:08 +0800

    Implement CustomStack

--- ECommerceSystem/DataStructuresLibrary/CustomStack.cs
+++ ECommerceSystem/DataStructuresLibrary/CustomStack.cs
@@ -3,21 +3,27 @@ namespace DataStructuresLibrary;
 public class CustomStack<T> where T : IComparable<T>
 {
     private readonly CustomArrayList<T> _items = new();
+
     public int Count => _items.Count;
 
     public void Push(T item)
     {
         _items.Add(item);
     }
+
     public T Pop()
     {
         if (Count == 0)
             throw new InvalidOperationException("Stack is empty.");
 
         T item = _items.Get(Count - 1);
-        _items.Remove(item);
+
+        if (!_items.Remove(item))
+            throw new InvalidOperationException("Unable to remove item.");
+
         return item;
     }
+
     public T Peek()
     {
         if (Count == 0)
@@ -25,14 +31,17 @@ public class CustomStack<T> where T : IComparable<T>
 
         return _items.Get(Count - 1);
     }
+
     public int Search(T item)
     {
         int index = _items.Search(item);
+
         if (index == -1)
             return -1;
 
         return Count - index;
     }
+
     public void Sort()
     {
         _items.Sort();
