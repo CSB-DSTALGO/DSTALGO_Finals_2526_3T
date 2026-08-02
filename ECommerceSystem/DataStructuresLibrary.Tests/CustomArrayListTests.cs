b32699e361cf093229083922b1651104a64bf2f3
kurt-11 <kurtjhervin09@gmail.com>
2026-08-02 20:24:17 +0800

    Update CustomArrayListTests.cs

--- ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
+++ ECommerceSystem/DataStructuresLibrary.Tests/CustomArrayListTests.cs
@@ -1,42 +1,105 @@
-namespace DataStructuresLibrary.Tests;
-
-using Xunit;
 using DataStructuresLibrary;
+
+namespace DataStructuresLibrary.Tests;
 
 public class CustomArrayListTests
 {
     [Fact]
-    public void Add_ShouldIncreaseCountAndStoreItems()
+    public void Add_ShouldIncreaseCount()
     {
-        // TODO: Implement test for Add and Get indexing
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+
+        Assert.Equal(2, list.Count);
     }
 
     [Fact]
-    public void Remove_ShouldShiftElementsCorrectly()
+    public void Get_ShouldReturnCorrectItem()
     {
-        // TODO: Implement test verifying element removal and index shifting
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+
+        Assert.Equal(20, list.Get(1));
     }
 
     [Fact]
-    public void Search_ShouldReturnCorrectIndex_WhenItemExists()
+    public void Search_ShouldReturnCorrectIndex()
     {
-        // TODO: Test Search returning zero-based index for existing element
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        Assert.Equal(1, list.Search(20));
     }
 
     [Fact]
     public void Search_ShouldReturnMinusOne_WhenItemDoesNotExist()
     {
-        // TODO: Test Search returning -1 when element is absent
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+
+        Assert.Equal(-1, list.Search(50));
     }
 
     [Fact]
-    public void Sort_ShouldOrderElementsInAscendingSequence()
+    public void Remove_ShouldRemoveItem()
     {
-        // TODO: Test Sort ordering an unsorted CustomArrayList<int>
-        throw new NotImplementedException();
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        bool result = list.Remove(20);
+
+        Assert.True(result);
+        Assert.Equal(2, list.Count);
+        Assert.Equal(30, list.Get(1));
+    }
+
+    [Fact]
+    public void Remove_ShouldReturnFalse_WhenItemDoesNotExist()
+    {
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+
+        bool result = list.Remove(50);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Sort_ShouldArrangeItemsInAscendingOrder()
+    {
+        var list = new CustomArrayList<int>();
+
+        list.Add(30);
+        list.Add(10);
+        list.Add(20);
+
+        list.Sort();
+
+        Assert.Equal(10, list.Get(0));
+        Assert.Equal(20, list.Get(1));
+        Assert.Equal(30, list.Get(2));
+    }
+
+    [Fact]
+    public void Get_ShouldThrowException_WhenIndexIsInvalid()
+    {
+        var list = new CustomArrayList<int>();
+
+        list.Add(10);
+
+        Assert.Throws<IndexOutOfRangeException>(() => list.Get(5));
     }
 }
